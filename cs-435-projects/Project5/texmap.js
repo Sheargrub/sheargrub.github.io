499f1bac68ab58748e1ecd0ec852d1b3271e8d7d
Sheargrub <sheargrub@gmail.com>
2024-12-12 16:51:37 -0600

    Debugging...

--- cs-435-projects/Project5/texmap.js
+++ cs-435-projects/Project5/texmap.js
@@ -278,9 +278,10 @@ class TableObj {
 
 function initTexture( image ) {
     texture = gl.createTexture();
+    console.log(image);
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
-        gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+        gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap(gl.TEXTURE_2D);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
                       gl.NEAREST_MIPMAP_LINEAR);
