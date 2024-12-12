2ac28cccb4cdc6bea7630ce746898d072ddbbc5e
Sheargrub <sheargrub@gmail.com>
2024-12-12 15:34:08 -0600

    Fix function (for real this time)

--- cs-435-projects/Project5/texmap.js
+++ cs-435-projects/Project5/texmap.js
@@ -205,7 +205,7 @@ function initVideoTexture() {
 
 function updateVideoTexture(gl, texture, video) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, video );
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video );
 }
 
 function disableVideoTexture(gl, texture) {
