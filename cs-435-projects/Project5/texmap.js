9109ab281ab09e9ca408d9cc58805902768f045a
Sheargrub <sheargrub@gmail.com>
2024-12-12 17:24:56 -0600

    Coords

--- cs-435-projects/Project5/texmap.js
+++ cs-435-projects/Project5/texmap.js
@@ -507,9 +507,9 @@ window.onload = function init() {
     document.getElementById("ButtonProj7").onclick = function(){window.open("http://sheargrub.com/435-Projects/Project7/proj7.html","_self");};
 
     // Init objects
-    tvScreen = new PlaneObj(0, -3, 0, 1.6, 0.9, 1, texVideo); 
+    tvScreen = new PlaneObj(0, 0, -1, 1.6, 0.9, 1, texVideo); 
     tvScreen.init();
-    table = new TableObj(0, -5, -0.25, 0.5, 0.5, 0.5, texWood);
+    table = new TableObj(0, -3, -2, 0.5, 0.5, 0.5, texWood);
     table.init();
 
     render();
