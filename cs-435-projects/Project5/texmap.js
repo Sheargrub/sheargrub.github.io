d82738fc9f9515682799f60a4f9c4726fccf4bb3
Sheargrub <sheargrub@gmail.com>
2024-12-12 18:52:46 -0600

    Resize tv

--- cs-435-projects/Project5/texmap.js
+++ cs-435-projects/Project5/texmap.js
@@ -530,9 +530,9 @@ window.onload = function init() {
     document.getElementById("ButtonProj7").onclick = function(){window.open("http://sheargrub.com/435-Projects/Project7/proj7.html","_self");};
 
     // Init objects
-    tvScreen = new PlaneObj(-0., 0, 2.5, 0.8, 0.45, 1, texVideo); 
+    tvScreen = new PlaneObj(-0.1, 0, 2.5, 1.1, 0.6, 1, texVideo); 
     tvScreen.init();
-    tvBody = new RectPrismObj(0, 0, 3.3, 0.9, 0.55, 0.3, texPlastic);
+    tvBody = new RectPrismObj(0, 0, 2.7, 1.3, 0.8, 0.3, texPlastic);
     tvBody.init();
     table = new TableObj(0, -1, 2, 1, 1, 1, texWood);
     table.init();
